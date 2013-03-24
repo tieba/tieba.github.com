022a8562b7d42b78f4347a993f00af02e09e8380
Peter Chen <peterctz2009@gmail.com>
2013-03-24 16:51:12 -0400

    update

--- common/content.js
+++ common/content.js
@@ -1,5 +1,5 @@
 common_info={
-	books:{name:"书籍简介(请注意分辨: C语言 或 C++ )",init:fill_node,content:[
+	books:{name:"书籍简介",init:fill_node,content:[
 		{name:"关于某些书",init:fill_info,content:[
 			["http://baike.baidu.com/view/963166.htm","<<C程序设计>> <sub>谭浩强(谭老湿) 著</sub>",
 				"一本挺<b>渣</b>的书,有一堆概念错误,误导性严重<sub>话说被误导的某些家伙经常会被喷的很惨</sub>"+"<br>"+
@@ -7,59 +7,95 @@ common_info={
 			],
 			["http://baike.baidu.com/view/963166.htm","<<C++程序设计>> <sub>谭浩强(谭老湿) 著</sub>","渣渣"],
 		],expanding:true,},
-		{name:"C语言入门书籍",init:fill_info,content:[
-			["","<<C Primer Plus>> <sub>by Stephen Prata</sub>","一本很经典的入门书籍<sub>据报告有不少打字错误</sub>"],
-			["","<<C语言入门经典>> <sub>by Ivor Horton</sub>","据说是本很经典的入门书籍<sub>ps.本人没看过</sub>"],
-			["http://baike.baidu.com/view/611688.htm","[TCPL]<<C程序设计语言>> <sub>by Kernighan &amp; Ritchie</sub>",
-				"传说中的K&R C, 具有无上尊严的某书<sub>C语言的创造者的书,ANSI标准起源于此,有点难度</sub>"
-			],
-		],width_f:350},
-		{name:"C语言进阶书籍",init:fill_info,content:[
-			["","<<C和指针>> <sub>by Kenneth Reek</sub>","对每个学C语言的人而言都是值得一看的好书<sub></sub>"],
-			["","<<C专家编程>> <sub>by Peter van der Linden</sub>","深入地讲解了C语言的一些方面<sub>看此书得有点基础</sub>"],
-			["","<<C陷阱与缺陷>> <sub>by Andrew Koenig</sub>","如何不想被bug整死,请看这里<sub>看完入门书之后可以花几天先浏览一遍,排毒养颜</sub>"],
+		{name:"C语言",init:fill_node,content:[
+			{name:"入门书籍",init:fill_info,content:[
+				["","<<C Primer Plus>> <sub>by Stephen Prata</sub>","一本很经典的入门书籍<sub>据报告有不少打字错误</sub>"],
+				["","<<C语言入门经典>> <sub>by Ivor Horton</sub>","据说是本很经典的入门书籍<sub>ps.本人没看过</sub>"],
+				["http://baike.baidu.com/view/611688.htm","[TCPL]<<C程序设计语言>> <sub>by Kernighan &amp; Ritchie</sub>",
+					"传说中的K&R C, 具有无上尊严的某书<sub>C语言的创造者的书,ANSI标准起源于此,有点难度</sub>"
+				],
+			],width_f:330},
+			{name:"进阶书籍",init:fill_info,content:[
+				["","<<C和指针>> <sub>by Kenneth Reek</sub>","对每个学C语言的人而言都是值得一看的好书<sub></sub>"],
+				["","<<C专家编程>> <sub>by Peter van der Linden</sub>","深入地讲解了C语言的一些方面<sub>看此书得有点基础</sub>"],
+				["","<<C陷阱与缺陷>> <sub>by Andrew Koenig</sub>","如何不想被bug整死,请看这里<sub>看完入门书之后可以花几天先浏览一遍,排毒养颜</sub>"],
+			]},
+		],expanding:true,},
+		{name:"C++",init:fill_node,content:[
+			{name:"入门书籍",init:fill_info,content:[
+				["","<<C++ Primer>> <sub>by Stanley B. Lippman...</sub>","一本非常经典的入门书籍<sub>需要一定的理论基础</sub>"],
+				["","<<C++ Primer Plus>> <sub>by Stephen Prata</sub>","挺不错的无基础入门书"],
+				["","<<C++程序设计语言>> <sub>by Bjarne Stroustrup</sub>","C++作者的书"],
+			]},
+			{name:"进阶书籍",init:fill_info,content:[
+				["","<<Thinking In C++>> <sub>by Bruce Eckel</sub>","C++编程思想, 适合研究研究"],
+				["","<<Modern C++ Design>> <sub>by Andrei Alexandrescu</sub>","关于模板元编程的(Warning:非常难)"],
+				["","<<Inside The C++ Object Model>> <sub>by Stanley B. Lippman</sub>","深入讲解了C++的对象模型"],
+				["","<<The Design and Evolution of C++>> <sub>by Bjarne Stroustrup</sub>","C++的发展史"],
+				["","<<Effective C++>> <sub>by Scott Meyers</sub>","详细的介绍了各种规则和坑"],
+				["","<<Exceptional C++>> <sub>by Herb Sutter</sub>","类似Effective C++"],
+				["","<<Effective STL>> <sub>by Scott Meyers</sub>","STL介绍"],
+				["","<<The C++ standard library>> <sub>by  Nicolai M. Josuttis</sub>","字典"],
+				["","<<Accelerated C++>> <sub>by Andrew Koenig...</sub>","..."],
+				["","<<Essential C++>> <sub>by Stanley B. Lippman</sub>","..."],
+				["","<<Exceptional C++ Style>> <sub>by Herb Sutter</sub>","风格问题"],
+				["","<<C++ Templates: The Complete Guide>> <sub>by David Vandevoorde...</sub>","模板百科"],
+				
+			],width_f:400},
+		],expanding:true,},
+		{name:"高阶内容(能看原版的去看原版去...)",init:fill_node,content:[
+			{name:"数学/算法/数据结构",init:fill_info,content:[
+				["","[CLRS]<<introduction to algorithms>> <sub>by C,L,R,S</sub>","算法导论"],
+				["","[TAOCP]<<The Art of Computer Programming>> <sub>by Donald Knuth</sub>","计算机程序设计艺术"],
+				["","<<Concrete Mathematics: A Foundation for Computer Science>> <sub>by Donald Knuth</sub>","具体数学"],
+			],width_f:600},
+			{name:"计算机系统",init:fill_info,content:[
+				["","[CSAPP]<<Computer Systems: A Programmer's Perspective>> <sub>by Randal E. Bryant...</sub>","深入理解计算机系统"],
+				["","[恐龙书]<<Operating System Concepts>> <sub>by Greg Gagne...</sub>","操作系统概念"],
+				["","<<Modern Operating Systems>> <sub>by Andrew S. Tanenbaum</sub>","现代操作系统"],
+				["","[APUE]<<Advanced Programming in the Unix Environment>> <sub>by W. Richard Stevens</sub>","UNIX环境高级编程"],
+				["","<<The UNIX Programming Envirment>> <sub>by Brian Kernighan</sub>","UNIX编程环境"],
+				["","<<The Art of Unix Programming>> <sub>by Eric S. Raymond</sub>","UNIX编程艺术"],
+				["","<<The Unix-Haters Handbook>> <sub>by Simson Garfinkel</sub>","UNIX痛恨者手册"],
+			],width_f:600},
+			{name:"网络编程",init:fill_info,content:[
+				["","<<TCP/IP Illustrated>> <sub>by W. Richard Stevens</sub>","TCP/IP 详解"],
+				["","<<Computer networks>> <sub>by Andrew S. Tanenbaum</sub>","计算机网络"],
+				["","<<Computer Networks and Internets>> <sub>by Douglas E. Comer</sub>","用TCP/IP进行网际互连"],
+				["","[UNP]<<UNIX Network Programming>> <sub>by W. Richard Stevens</sub>","UNIX网络编程"],
+			],width_f:600},
+			{name:"计算机程序/编译原理",init:fill_info,content:[
+				["","[DragonBook/龙书]<<Compilers: Principles, Techniques, and Tools>> <sub>by Alfred Aho...</sub>","编译原理"],
+				["","[SICP]<<Structure and Interpretation of Computer Programs>> <sub>by Gerald Jay Sussman</sub>","计算机程序的构造和解释"],
+			],width_f:650},
+			{name:"其他",init:fill_info,content:[
+				["","[GoF]<<Design Patterns:Elements of Reusable Object-Oriented Software>> <sub>by Gang of Four</sub>","设计模式"],
+				["","<<The Practice of Programming>> <sub>by Brian Kernighan...</sub>","程序设计实践"],
+			],width_f:700},
+		],expanding:true,},
+		{name:"其他",init:fill_node,content:[
+			{name:"Java",init:fill_info,content:[
+				["","<<Thinking in Java>> <sub>by Bruce Eckel</sub>",""],
+			]},
+			{name:"noip",init:fill_info,content:[
+				["","<<算法竞赛入门经典>> <sub>by 刘汝佳</sub>",""],
+			]},
 		]},
-		
-		{name:"C++入门书籍",init:fill_info,content:[
-			["","<<C++ Primer>> <sub>by Stanley B. Lippman...</sub>","一本非常经典的入门书籍<sub>需要一定的理论基础</sub>"],
-			["","<<C++ Primer Plus>> <sub>by Stephen Prata</sub>","挺不错的无基础入门书"],
-			["","<<C++程序设计语言>> <sub>by Bjarne Stroustrup</sub>","C++作者的书"],
+		{name:"暫缺",init:fill_info,content:[
+			["","Windows相关物","Windows kernel/MFC..."],
+			["","GUI","Qt/Gtk/Tk/Motif..."],
+			["","计算机图形","专业书籍/OpenGL/DirectX"],
+			["","移动平台","IOS/Android/..."],
+			["","分布式/并行",""],
+			["","嵌入式",""],
+			["","人工智能",""],
+			["","硬件组成",""],
+			["","信息安全",""],
+			["","数学","编码理论,图论,数论,博弈论，逻辑学"],
+			["","算法/数据结构",""],
+			["","数据库",""],
+			["","编程范式",""],
 		]},
-		{name:"C++进阶书籍",init:fill_info,content:[
-			["","<<C++编程思想(Thinking In C++)>> <sub>by Bruce Eckel</sub>","适合研究研究"],
-			["","<<Modern C++ Design>> <sub>by Andrei Alexandrescu</sub>",""],
-			["","<<Inside The C++ Object Model>> <sub>by Stanley B. Lippman</sub>",""],
-			["","<<Effective C++>> <sub>by Scott Meyers</sub>",""],
-			["","<<Effective STL>> <sub>by Scott Meyers</sub>",""],
-			["","<<The C++ standard library>> <sub>by  Nicolai M. Josuttis</sub>",""],
-			["","<<Exceptional C++>> <sub>by Herb Sutter</sub>",""],
-			["","<<Accelerated C++>> <sub>by Andrew Koenig...</sub>",""],
-			["","<<Essential C++>> <sub>Stanley B. Lippman</sub>",""],
-			["","<<The C++ Standard Library>> <sub>Nicolai M. Josuttis</sub>",""],
-			
-		],width_f:400},
-		{name:"高阶内容(能看原版的去看原版去...)",init:fill_info,content:[
-			["","[CLRS]<<算法导论>> <sub>by C,L,R,S</sub>","算法方面的神书"],
-			["","[APUE]<<UNIX环境高级编程>> <sub>by W. Richard Stevens</sub>",""],
-			["","[CSAPP]<<深入理解计算机系统>> <sub>by Randal E. Bryant...</sub>",""],
-			["","[UNP]<<UNIX网络编程>> <sub>by W. Richard Stevens</sub>",""],
-			["","[TAOCP]<<计算机程序设计艺术>> <sub>by Donald Knuth</sub>",""],
-			["","[DragonBook/龙书]<<编译原理>> <sub>by Alfred Aho...</sub>",""],
-			["","[恐龙书]<<操作系统概念>> <sub>by Greg Gagne...</sub>",""],
-			["","<<具体数学>> <sub>by Donald Knuth...</sub>",""],
-			["","<<现代操作系统>> <sub>by Andrew S. Tanenbaum</sub>",""],
-			["","<<TCP/IP 详解>> <sub>by W. Richard Stevens</sub>",""],
-			["","<<UNIX编程艺术>> <sub>by Eric S. Raymond</sub>",""],
-			["","<<UNIX痛恨者手册>> <sub>by Simson Garfinkel</sub>",""],
-			["","<<UNIX编程环境>> <sub>by Brian Kernighan</sub>",""],
-			["","[GangOfFour]<<设计模式>> <sub>by Gang of Four</sub>","争论很严重..."],
-			["","<<编程珠玑>> <sub>by Jon Bentley</sub>",""],
-			["","[SICP]<<计算机程序的构造和解释>> <sub>by Gerald Jay Sussman</sub>",""],
-			["","<<计算机网络>> <sub>by Andrew S. Tanenbaum</sub>",""],
-			["","<<用TCP/IP进行网际互连>> <sub>by Douglas E. Comer</sub>",""],
-			["","<<程序设计实践>> <sub>by Brian Kernighan...</sub>",""],
-			["","其他的请自己去Google/Baidu/Douban",""],
-		],width_f:400},
 	]},
 	hardware:{name:"关于硬件要求问题[仅参考]",init:fill_table,content:[
 		["","CPU","内存","硬盘空间","显卡"],

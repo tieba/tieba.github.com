e54402dc61c3e18c95050f23ddb905e88591c06a
Peter Chen <peterctz2009@gmail.com>
2013-06-13 20:11:12 -0400

    clean the html code

--- javascripts/table.js
+++ javascripts/table.js
@@ -11,20 +11,39 @@ function init_tbody(info){
 	return $("<tbody></tbody>").appendTo($('<table width="100%" class="section_content"></table>').appendTo(info.div));
 }
 
+function fill_rule(info){
+	var rules=info.content;
+	for(var i=0;i<rules.length;++i){
+		info.div.addClass("section_content")
+		info.div.append('<h4>'+rules[i][0]+'</h4>'+rules[i].slice(1).map(function(s){return "&nbsp;&nbsp;&nbsp;&nbsp;"+s+"<br>";}).join(""));
+	}
+}
+
+
 function fill_table(info){
+	var lnk=function(s){return '<a target="_blank" href="/i/sys/jump?un='+s+'">'+s+'</a>';}
 	var tbody=init_tbody(info)
 	var table=info.content;
 	for(var i=0;i!=table.length;++i){
 		var content=table[i];
-		var tr=$("<tr></tr>").appendTo(tbody);
-		for(var j=0;j!=content.length;++j){
-				$("<td align=\"left\" "+
-					(j==content.length-1?"colspan=\"100\"":"")+
-					"></td>").appendTo(tr)
-					.append(content[j]);
-		}
+		var tr=$('<tr valign="top"><td width="40px" align="left">'+content[0]+':</td><td>'+
+			content[1].map(lnk).join(" ")+
+		'</td></tr>').appendTo(tbody);
 	}
 }
+
+function fill_admin(info){
+	var lnk=function(s){return '<a target="_blank" href="/i/sys/jump?un='+s+'">'+s+'</a>';}
+	var tbody=init_tbody(info)
+	var table=info.content;
+	for(var i=0;i!=table.length;++i){
+		var content=table[i];
+		var tr=$('<tr valign="top"><td width="40px" align="left">'+content[0]+':</td><td>'+
+			content[1].map(lnk).join(" ")+
+		'</td></tr>').appendTo(tbody);
+	}
+}
+
 function fill_info(info){
 	var tbody=init_tbody(info)
 	var table=info.content;
@@ -110,6 +129,8 @@ function fill_node(info){
 function insert_table(){
 	for(var i=0;i!=infos.length;++i){
 		try{
+			if(infos[i].id)
+				$("<a name=\""+infos[i].id+"\"></a> ").appendTo("#tablearea");
 			$('<a href="javascript:;" onclick="toggle_table(this);" class="topic_title_link"><h3>'+infos[i].name+'</h3></a>').appendTo("#tablearea")
 			infos[i].div=$('<div class="topic_table_div"></div>').appendTo("#tablearea")
 			if(infos[i].init){
@@ -127,13 +148,19 @@ function insert_table(){
 } 
 
 
+function load_rule(div,rules){
+	for(var i=0;i<rules.length;++i){
+		div.append('<h4>'+rules[i][0]+'</h4>'+rules[i].slice(1).map(function(s){return "&nbsp;&nbsp;&nbsp;&nbsp;"+s+"<br>";}).join(""));
+	}
+}
+
 function init_page(){
-	load_admin($("#content_admin"))
-	load_rule($("#content_rule"))
-	toggle_table("#admin_title_link")
-	toggle_table("#rule_title_link")
+	//load_admin($("#content_admin"))
+	//load_rule($("#content_rule"),rules)
+	//toggle_table("#admin_title_link")
+	//toggle_table("#rule_title_link")
 	insert_table()
-	
+	/*
 	if(window.localStorage&&window.localStorage.how_to_ask_presented){
 		//$("#how_to_ask_link").css("display","block");
 	}else{
@@ -144,5 +171,5 @@ function init_page(){
 			$("#howtoask_link").slideToggle("fast",function(){$("#howtoask_link").remove()});
 			$("#how_to_ask_link").slideToggle();
 		},5000);
-	}
+	}*/
 }

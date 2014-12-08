450589430e179ae5927f25c4dc1782f738c991b3
FrankHB <frankhb1989@gmail.com>
2014-12-08 14:17:47 +0800

    update.sh: Hash "python3" to prevent overwriting output when python3 executable not found.
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #/usr/bin/env bash
 
+set -e
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
+
+hash python3
 
 python3 ./common/gen.py <./cpl/content.json >./cpl/index.html
 python3 ./common/gen.py --mobile <./cpl/content.json >./cpl/mobile.html

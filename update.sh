7d387fb09126631afb797feae658427a21bf3cde
FrankHB <frankhb1989@gmail.com>
2014-12-08 14:17:05 +0800

    update.sh: Kept LF at EOF.
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #/usr/bin/env bash
+
 python3 ./common/gen.py <./cpl/content.json >./cpl/index.html
 python3 ./common/gen.py --mobile <./cpl/content.json >./cpl/mobile.html
 python3 ./common/gen.py <./cpp/content.json >./cpp/index.html
 python3 ./common/gen.py --mobile <./cpp/content.json >./cpp/mobile.html
+
